0661e614923c2dcef61310450b22b4fd730e78ca
Ameer Elattar <amer.alattar.ae@gmail.com>
2024-05-15 04:48:25 +0300

    removing pics

--- frontend-with-vuejs/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './route';
-import {createPinia} from 'pinia';
-
-const pinia = createPinia();
-
-createApp(App).use(pinia).use(router).mount('#app')
